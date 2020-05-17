a8ad4febc2abf1080d14ef524afb938d5e1e59e3
AlanLee-Java <19865879289@163.com>
2020-05-17 18:05:46 +0800

    初始化项目

---
+++ src/main/java/com/alanlee/BootRedisDistributedLockApplication.java
@@ -0,0 +1,13 @@
+package com.alanlee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootRedisDistributedLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootRedisDistributedLockApplication.class, args);
+    }
+
+}
